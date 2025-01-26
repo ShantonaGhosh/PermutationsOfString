628f7e1614df7a1600e247a1673ff27157366d09
ShantonaGhosh <58144549+ShantonaGhosh@users.noreply.github.com>
2025-01-26 22:51:43 +0600

    Permutations  of string

---
+++ src/main/java/com/myjavaprogram/permutationsofstring/PermutationsOfString.java
@@ -0,0 +1,26 @@
+package com.myjavaprogram.permutationsofstring;
+
+public class PermutationsOfString {
+//Example : 3!=6,4!=24 Factorial
+    public static void printPerm(String str, String permutation) {
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            
+            //"abc" -> "ab"
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            
+            printPerm(newStr, permutation + currChar);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printPerm(str, "");
+    }
+}
